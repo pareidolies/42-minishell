57ddd89670e7cafda84e4683a214676c23ae7369
Lea MURTIN <lmurtin@e1r5p19.42.fr>
2022-07-06 17:11:52 +0200

    début affichage et execution

---
+++ testmain.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+/*test affichage du prompt et recup de la ligne*/
+int main(void)
+{
+	char	*result;
+	int		flag;
+	
+	while (flag != 1) 
+	{
+	result = readline("minishell>> ");
+	printf("User said : [%s]\n", result);
+	add_history(result);
+	if (strcmp(result, "aurevoir") == 0)
+		flag = 1;
+	free(result);
+	}
+	rl_clear_history();
+	return (0);
+}
+
+/*Une fois que le parser nous a permis de recup un tableau de string*/
+/*constitué de la commande et ses argument :*/
+int	exe_command(char **command)
+{
+	char	*path_var;
+
+	/*savoir si on a déjà un chemin ou si on doit aller le chercher dans l'env*/
+	if (command[0] contient un '/')
+		traiter_absolute_relative_path(command[0]);
+	else
+	{
+		path_var = getenv("PATH");
+		printf("key = PATH, value = [%s]\n", path);
+	}
+}
