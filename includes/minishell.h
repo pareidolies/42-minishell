260c3e325d3e0ea2e50581bed822d392d67c5f41
pareidolies <sacha.mkara@gmail.com>
2022-07-10 18:46:44 +0200

    thinking about the different steps of the parsing

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,6 +6,51 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+/******************************************************************************
+ *                               STRUCTURES                                   *
+ *****************************************************************************/
+
+typedef struct s_command_table 
+{
+    t_list *commands;
+    char	**env;
+	pid_t	pid;
+}   t_command_table;
+
+typedef struct s_command
+{
+    char	*cmd;
+	char	*path;
+    char    *parameters;
+	int		infile; //default stdin
+	int		outfile; //default stdout
+    struct s_command *next;
+    struct s_command *prev;
+}   t_command;
+
+typedef struct s_token
+{
+    char    *token;
+    int     flag;
+    struct s_token *next;
+    struct s_token *prev;
+}   t_token;
+
+/******************************************************************************
+ *                             ENUMERATIONS                                   *
+ *****************************************************************************/
+
+
+/******************************************************************************
+ *                            GLOBAL VARIABLE                                 *
+ *****************************************************************************/
+
+int g_exit_status;
+
+/******************************************************************************
+ *                               FUNCTIONS                                    *
+ *****************************************************************************/
+
 //testmain.c
 int	exe_command(char **commandfull);
 environment_path(char *command, char *path_var);
