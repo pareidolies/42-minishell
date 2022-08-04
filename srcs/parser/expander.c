359e438e30619bcda7f21125a1a533bb68c94d41
pareidolies <sacha.mkara@gmail.com>
2022-08-04 12:34:14 +0200

    norminette

--- srcs/parser/expander.c
+++ srcs/parser/expander.c
@@ -1,39 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expander.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smostefa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/04 12:24:37 by smostefa          #+#    #+#             */
+/*   Updated: 2022/08/04 12:33:58 by smostefa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-/* If many variables to expand : ft_split ($) */
-
-//useless now
-
-/*int there_is_a_dollar(char *str)
-{
-    int i;
-
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] == '$')
-            return (1);
-        i++;
-    }
-    return (0);
-}
-
-//useless now
-
-void    analyze_tokens_to_expand(t_token *list)
-{
-    t_token *current;
-
-    current = list;
-    while (current != NULL)
-    {
-        if (current->token[0] != S_QUOTE && there_is_a_dollar(current->token))
-            current->to_expand = 1;
-        current = current->next;
-    }
-}*/
-
-int		is_in_d_quote(char *str, int pos)
+int	is_in_d_quote(char *str, int pos)
 {
 	int	i;
 	int	result;
@@ -62,13 +41,13 @@ int		is_in_d_quote(char *str, int pos)
 		}
 		i++;
 	}
-    if (result == 2)
-        return (2);
-    else
-	    return (0);
+	if (result == 2)
+		return (2);
+	else
+		return (0);
 }
 
-int		is_in_s_quote(char *str, int pos)
+int	is_in_s_quote(char *str, int pos)
 {
 	int	i;
 	int	result;
@@ -97,82 +76,87 @@ int		is_in_s_quote(char *str, int pos)
 		}
 		i++;
 	}
-    if (result == 1)
-        return (1);
-    else
-	    return (0);
+	if (result == 1)
+		return (1);
+	else
+		return (0);
 }
 
-int     get_expanded_token_start(char *str, char *initial, int pos)
+int	get_expanded_token_start(char *str, char *initial, int pos)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] == DOLLAR && !is_in_s_quote(initial, pos) && str[i + 1] && (!is_in_d_quote(initial, pos) || ft_isalpha(str[i + 1])) && str[i + 1] != SPACE && str[i + 1] != '=')
-            break;
-        i++;
-    }
-    return (i);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == DOLLAR && !is_in_s_quote(initial, pos)
+			&& str[i + 1] && (!is_in_d_quote(initial, pos)
+			|| ft_isalpha(str[i + 1])) && str[i + 1] != SPACE && str[i + 1] != '=')
+		break ;
+		i++;
+	}
+	return (i);
 }
 
-int     get_expanded_token_size(char *str)
+int	get_expanded_token_size(char *str)
 {
-    int i;
+	int i;
 
-    i = 0;
-    if (!str[i])
-        return (1);
-    if (str[i] == S_QUOTE || str[i] == D_QUOTE)
-        return (0);
-    if (ft_isdigit(str[i]))
-        return (1);
-    while (str[i] && str[i] != DOLLAR && str[i] != S_QUOTE && str[i] != D_QUOTE && str[i] != SPACE && str[i] != ']' && str[i] != '%' && str[i] != '=')
-    {
-        if (str[i] == QUESTION)
-            return (1);
-        i++;
-    }
-    return (i);
+	i = 0;
+	if (!str[i])
+		return (1);
+	if (str[i] == S_QUOTE || str[i] == D_QUOTE)
+		return (0);
+	if (ft_isdigit(str[i]))
+		return (1);
+	while (str[i] && str[i] != DOLLAR && str[i] != S_QUOTE
+		&& str[i] != D_QUOTE && str[i] != SPACE
+		&& str[i] != ']' && str[i] != '%' && str[i] != '=')
+	{
+		if (str[i] == QUESTION)
+		return (1);
+		i++;
+	}
+	return (i);
 }
 
-char    *get_expanded_key(char *str, int size, t_env *envlist)
+char	*get_expanded_key(char *str, int size, t_env *envlist)
 {
-    //printf("substr : %s\n", ft_substr(str, 0, size));
-    char    *key;
-    char    *res;
+	char *key;
+	char *res;
 
-    key = ft_substr(str, 0, size);
-    magic_malloc(ADD, 0, key);
-    res = ft_getenv(key, envlist);
-    magic_malloc(FREE, 0, key);
-    return(res);
+	key = ft_substr(str, 0, size);
+	magic_malloc(ADD, 0, key);
+	res = ft_getenv(key, envlist);
+	magic_malloc(FREE, 0, key);
+	return(res);
 }
 
-char    *create_expanded_token(char *str, t_env *envlist)
+char	*create_expanded_token(char *str, t_env *envlist)
 {
-    char    *result;
-    int     i;
-    int     size;
-    int     before_dollar;
-    char    *tmp;
-    char    *substring;
+	char 	*result;
+	int	i;
+	int	size;
+	int	before_dollar;
+	char	*tmp;
+	char	*substring;
 
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] == DOLLAR && !is_in_s_quote(str, i) && str[i + 1] && (!is_in_d_quote(str, i) || ft_isalpha(str[i + 1])) && str[i + 1] != SPACE && str[i + 1] != '=')
-            break;
-        i++;
-    }
-    result = ft_substr(str, 0, i);
-    magic_malloc(ADD, 0, result);
-    //printf("*result* step 1 : %s\n", result);
-    while(str[i])
-    {
-        i++;
-        size = get_expanded_token_size(&str[i]);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == DOLLAR && !is_in_s_quote(str, i)
+			&& str[i + 1] && (!is_in_d_quote(str, i)
+			|| ft_isalpha(str[i + 1]))
+			&& str[i + 1] != SPACE && str[i + 1] != '=')
+		break ;
+		i++;
+	}
+	result = ft_substr(str, 0, i);
+	magic_malloc(ADD, 0, result);
+	while(str[i])
+	{
+		i++;
+		size = get_expanded_token_size(&str[i]);
         //printf("size : %d\n", size);
         /*if (!str[i])
         {
@@ -181,38 +165,38 @@ char    *create_expanded_token(char *str, t_env *envlist)
             magic_malloc(FREE, 0, result);
             return (tmp);
         }*/
-        if (!str[i])
-            break;
-        tmp = ft_strjoin(result, get_expanded_key(&str[i], size, envlist));
-        magic_malloc(ADD, 0, tmp);
-        magic_malloc(FREE, 0, result);
-        //printf("*result* step 2 : %s\n", result);
-        i = i + size;
-        before_dollar = get_expanded_token_start(&str[i], str, i);
-        //printf("before dollar : %d\n", before_dollar);
-        substring = ft_substr(&str[i], 0, before_dollar);
-        magic_malloc(ADD, 0, substring);
-        result = ft_strjoin(tmp, substring);
-        magic_malloc(ADD, 0, result);
-        magic_malloc(FREE, 0, tmp);
-        magic_malloc(FREE, 0, substring);
-        //printf("*result* step 3 : %s\n", result);
-        i = i + before_dollar;
-    }
-    return (result);
+	if (!str[i])
+		break ;
+	tmp = ft_strjoin(result, get_expanded_key(&str[i], size, envlist));
+	magic_malloc(ADD, 0, tmp);
+	magic_malloc(FREE, 0, result);
+	//printf("*result* step 2 : %s\n", result);
+	i = i + size;
+	before_dollar = get_expanded_token_start(&str[i], str, i);
+	//printf("before dollar : %d\n", before_dollar);
+	substring = ft_substr(&str[i], 0, before_dollar);
+	magic_malloc(ADD, 0, substring);
+	result = ft_strjoin(tmp, substring);
+	magic_malloc(ADD, 0, result);
+	magic_malloc(FREE, 0, tmp);
+	magic_malloc(FREE, 0, substring);
+	//printf("*result* step 3 : %s\n", result);
+	i = i + before_dollar;
+	}
+	return (result);
 }
 
-void    expander(t_token *list, t_env *envlist)
+void	expander(t_token *list, t_env *envlist)
 {
-    t_token *current;
+	t_token	*current;
 
-    current = list;
-    while (current != NULL)
-    {
+	current = list;
+	while (current != NULL)
+	{
         //if (current->to_expand)
-            current->expanded_token = create_expanded_token(current->token, envlist);
+		current->expanded_token = create_expanded_token(current->token, envlist);
         //else
         //    current->expanded_token = current->token;
-        current = current->next;
-    }
+		current = current->next;
+	}
 }
