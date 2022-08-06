3196f75bb552b75934de0603e5b824008fc9f349
pareidolies <sacha.mkara@gmail.com>
2022-08-06 18:48:42 +0200

    norminette

--- srcs/parser/splitter.c
+++ srcs/parser/splitter.c
@@ -12,6 +12,29 @@
 
 #include "minishell.h"
 
+int	calculate_result(int result, char c)
+{
+	if (c == S_QUOTE)
+	{
+		if (result == 0)
+			return (1);
+		else if (result == 1)
+			return (0);
+		else if (result == 2)
+			return (2);
+	}
+	if (c == D_QUOTE)
+	{
+		if (result == 0)
+			return (2);
+		else if (result == 2)
+			return (0);
+		else if (result == 1)
+			return (1);
+	}
+	return (result);
+}
+
 int	is_in_quote(char *str, int pos)
 {
 	int	i;
@@ -21,24 +44,8 @@ int	is_in_quote(char *str, int pos)
 	i = 0;
 	while (str[i] && i < pos)
 	{
-		if (str[i] == S_QUOTE)
-		{
-			if (result == 0)
-				result = 1;
-			else if (result == 1)
-				result = 0;
-			else if (result == 2)
-				result = 2;
-		}
-		if (str[i] == D_QUOTE)
-		{
-			if (result == 0)
-				result = 2;
-			else if (result == 2)
-				result = 0;
-			else if (result == 1)
-				result = 1;
-		}
+		if (str[i] == S_QUOTE || str[i] == D_QUOTE)
+			result = calculate_result(result, str[i]);
 		i++;
 	}
 	return (result);
