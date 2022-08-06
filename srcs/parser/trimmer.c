9faf4b06749de2efedc8a258ac21e22bb3f8397f
pareidolies <sacha.mkara@gmail.com>
2022-08-06 19:12:23 +0200

    norminette

--- srcs/parser/trimmer.c
+++ srcs/parser/trimmer.c
@@ -12,13 +12,40 @@
 
 #include "minishell.h"
 
+char	*get_substring(char *str, int *i, int start, char c, char *tmp)
+{
+	char	*substring;
+	char	*result;
+
+	if (c == S_QUOTE || c == D_QUOTE)
+	{
+		(*i)++;
+		while (str[*i] && str[*i] != c)
+			(*i)++;
+		substring = ft_substr(str, start + 1, *i - start - 1);
+		magic_malloc(ADD, 0, substring);
+		result = ft_strjoin(tmp, substring);
+		magic_malloc(FREE, 0, substring);
+		(*i)++;
+	}
+	else
+	{
+		while (str[*i] && str[*i] != S_QUOTE && str[*i] != D_QUOTE)
+				(*i)++;
+		substring = ft_substr(str, start, *i - start);
+		magic_malloc(ADD, 0, substring);
+		result = ft_strjoin(tmp, substring);
+		magic_malloc(FREE, 0, substring);
+	}
+	return (result);
+}
+
 char	*withdraw_quotes(char *str)
 {
 	int		i;
 	char	*result;
 	int		start;
 	char	*tmp;
-	char	*substring;
 
 	tmp = ft_strdup("");
 	magic_malloc(ADD, 0, tmp);
@@ -26,43 +53,9 @@ char	*withdraw_quotes(char *str)
 	while (str[i])
 	{
 		start = i;
-		if (str[i] == S_QUOTE)
-		{
-			i++;
-			while (str[i] && str[i] != S_QUOTE)
-				i++;
-			substring = ft_substr(str, start + 1, i - start - 1);
-			magic_malloc(ADD, 0, substring);
-			result = ft_strjoin(tmp, substring);
-			magic_malloc(ADD, 0, result);
-			magic_malloc(FREE, 0, substring);
-			magic_malloc(FREE, 0, tmp);
-			i++;
-		}
-		else if (str[i] == D_QUOTE)
-		{
-			i++;
-			while (str[i] && str[i] != D_QUOTE)
-				i++;
-			substring = ft_substr(str, start + 1, i - start - 1);
-			magic_malloc(ADD, 0, substring);
-			result = ft_strjoin(tmp, substring);
-			magic_malloc(ADD, 0, result);
-			magic_malloc(FREE, 0, substring);
-			magic_malloc(FREE, 0, tmp);
-			i++;
-		}
-		else
-		{
-			while (str[i] && str[i] != S_QUOTE && str[i] != D_QUOTE)
-				i++;
-			substring = ft_substr(str, start, i - start);
-			magic_malloc(ADD, 0, substring);
-			result = ft_strjoin(tmp, substring);
-			magic_malloc(ADD, 0, result);
-			magic_malloc(FREE, 0, substring);
-			magic_malloc(FREE, 0, tmp);
-		}
+		result = get_substring(str, &i, start, str[i], tmp);
+		magic_malloc(ADD, 0, result);
+		magic_malloc(FREE, 0, tmp);
 		tmp = ft_strdup(result);
 		magic_malloc(ADD, 0, tmp);
 		magic_malloc(FREE, 0, result);
