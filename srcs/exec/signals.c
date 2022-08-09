76f9bfb57ed088002b38ec8604e317e04e960db4
pareidolies <sacha.mkara@gmail.com>
2022-08-09 11:23:44 +0200

    cleaning

--- srcs/exec/signals.c
+++ srcs/exec/signals.c
@@ -6,13 +6,13 @@
 /*   By: smostefa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 15:53:05 by smostefa          #+#    #+#             */
-/*   Updated: 2022/08/07 15:53:07 by smostefa         ###   ########.fr       */
+/*   Updated: 2022/08/09 11:22:48 by smostefa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-extern int g_exit_status;
+extern int	g_exit_status;
 
 int	set_signals_as_prompt(void)
 {
@@ -31,22 +31,22 @@ int	set_signals_as_here_doc(void)
 void	signal_handler_as_prompt(int signum)
 {
 	if (signum == SIGINT)
-    {
+	{
 		g_exit_status = 130;
 		//ft_putstr_fd("\n", 1);
 		ioctl(STDIN_FILENO, TIOCSTI, "\n");
 		rl_replace_line("", 0);
 		rl_on_new_line();
-    }
+	}
 }
 
 void	signal_handler_as_here_doc(int signum)
 {
 	if (signum == SIGINT)
-    {
+	{
 		g_exit_status = 130;
 		magic_malloc(130, 0, NULL);
-    }
+	}
 }
 
 //ajouter signal handler as parent and children ?
